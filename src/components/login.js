dddd24e3876b5ff99cc04b91ee18ab7394bf4a2a
Avneesh dwivedi <avndvd2409@gmail.com>
2020-01-29 08:13:21 +0530

    css of login page changed

--- src/components/login.js
+++ src/components/login.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
-import style from '../login.module.css'
-
+import '../login.module.css'
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmitHandler = () => {
+        console.log(this.state)
+    }
     render() {
         return (
             <div className="form-wrapper">
-                <form action="" method="post">
+                <div>
                     <h3>Login here</h3>
                     <div className="form-item">
-                        <input type="text" name="username" placeholder="Username" autofocus required></input>
+                        <input type="text" name="username" placeholder="Username" onChange={this.changeHandler} />
                     </div>
                     <div className="form-item">
-                        <input type="password" name="password" placeholder="Password" required></input>
+                        <input type="password" name="password" placeholder="Password" onChange={this.changeHandler} />
                     </div>
                     <div className="button-panel">
-                        <input type="submit" className="button" title="Log In" name="login" value="Login"></input>
+                        <input type="submit" className="button" title="Log In" name="login" value="Login" />
                     </div>
-                </form>
+                </div>
                 <div className="reminder">
                     <p>Not a member? <a href="/register">Sign up now</a></p>
                     <p><a href="/">Forgot password?</a></p>
